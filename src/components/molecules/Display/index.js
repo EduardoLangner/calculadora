9f083d3d32249f508ef447e995ef1aa3895b5b89
EduardoLangner <eduardodossantos752@gmail.com>
2022-10-13 14:56:13 -0300

    Calculadora Iphone

---
+++ src/components/molecules/Display/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css'
+
+const display = (props) => {
+    return (
+        <div className='display'>{props.value}</div>
+    )
+}
+
+export default display
